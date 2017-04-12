2effc5ff5dd4d164df7a6692d1eee1c0a8acc88d
Steve Bentley <srbentley12@gmail.com>
2017-04-12 15:33:31 -0700

    Alpha with text to speech V 0.1.1

--- app/src/main/java/com/example/steve/flashcards2/MainActivity.java
+++ app/src/main/java/com/example/steve/flashcards2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.steve.flashcards2;
 
 import android.content.res.AssetManager;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,15 +18,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
     List<String> lines = new ArrayList<String>();
     private String[] cities;
     private String[] codes;
     private int[] index;
     private int currentIndex = 0;
-
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,10 +50,54 @@ public class MainActivity extends AppCompatActivity {
             cities[i] = lines.get(2*i);
             codes[i] = lines.get((2*i) + 1);
         }
+
+        tts = new TextToSpeech(this, this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         shuffle();
+        Button buttonClick = (Button) findViewById(R.id.nextButton);
+        Button buttonShuffleClick = (Button) findViewById(R.id.reshuffleButton);
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView city = (TextView) findViewById(R.id.cityTextView);
+                TextView code = (TextView) findViewById(R.id.codeTextView);
+                if(code.getText() == ""){
+                    code.setText(codes[index[currentIndex]]);
+                    speakOut(code);
+                }
+                else if(currentIndex == index.length - 1) {
+                    shuffle();
+                }
+                else {
+                    currentIndex++;
+                    city.setText(cities[index[currentIndex]]);
+                    code.setText("");
+                    speakOut(city);
+                }
+            }
+        });
+
+        buttonShuffleClick.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                shuffle();
+            }
+        });
     }
+
+    @Override
+    public void onInit(int status) {
+        tts.setLanguage(Locale.US);
+        tts.setSpeechRate((float) 0.8);
+        tts.setPitch((float) 0.8);
+        }
+
+        public void speakOut(TextView text) {
+            CharSequence ttsText = text.getText();
+            tts.speak(ttsText, TextToSpeech.QUEUE_FLUSH, null, "id1");
+        }
+
 
     public void readLines() throws IOException {
         AssetManager assetManager = getAssets();
@@ -63,26 +110,6 @@ public class MainActivity extends AppCompatActivity {
         bufferedReader.close();
     }
 
-    public void buttonClick(View view){
-        shuffle();
-    }
-
-    public void nextClick(View view){
-        TextView city = (TextView) findViewById(R.id.cityTextView);
-        TextView code = (TextView) findViewById(R.id.codeTextView);
-        if(code.getText() == ""){
-            code.setText(codes[index[currentIndex]]);
-        }
-        else if(currentIndex == index.length - 1) {
-            shuffle();
-        }
-        else {
-            currentIndex++;
-            city.setText(cities[index[currentIndex]]);
-            code.setText("");
-        }
-    }
-
     public void shuffle(){
         currentIndex = 0;
         shuffleArray(index);
@@ -90,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
         city.setText(cities[index[currentIndex]]);
         TextView code = (TextView) findViewById(R.id.codeTextView);
         code.setText("");
+        speakOut(city);
     }
 
     public static void shuffleArray(int[] arr) {
